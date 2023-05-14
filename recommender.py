168b768f6c7e56d6876172740253afac173ecfb4
entlo <alopez33@terpmail.umd.edu>
2023-05-13 22:58:55 -0400

    Update recommender.py
    updated main function, added an argument in parse_args and main function call

--- recommender.py
+++ recommender.py
@@ -148,39 +148,112 @@ class Database:
             print(f"Section{section_number} of course already exists in database")
           
 
-def main(words):
+def main(keywords, department_list):
     catalog = Catalog(
         url="https://app.testudo.umd.edu/soc/",
         driver_path="./chromedriver.exe",
         search_bar_id="course-id-input",
         search_button_id="search-button"
     )
-    department_list = []
-    time.sleep(3)
-    departments = catalog.driver.find_elements(By.XPATH, ".//div[@id='course-prefixes-page']//div[@class='course-prefix row']")
-    for department in departments:
-        department_list.append(department.find_element(By.XPATH, ".//span[@class='prefix-abbrev push_one two columns']").text)
+    db = Database("courses.db")
+    db.create_tables()
 
-    # Search for each department and parse the course info based on keywords
+    # Get all the courses and their descriptions for the given departments
+    courses_data = []
     for department_name in department_list:
         # Search for the department
         catalog.search(department_name)
-        time.sleep(3)
+        time.sleep(1)
 
         # Get all the courses
         courses = catalog.driver.find_elements(By.XPATH, ".//div[@class='course']")
 
-        # Parse each course for keywords
+        # Parse each course for course data
         for course in courses:
-            # Get the course title and description
-            title = course.find_element(By.XPATH, ".//span[@class='course-title']").text
-            description = course.find_element(By.XPATH, "//div[@class='row']//div[@class='approved-course-text']").text
+            try:
+                show_sections_button = course.find_element(By.XPATH, ".//a[@class='toggle-sections-link']")
+                if show_sections_button.is_displayed():
+                    show_sections_button.click()
+                time.sleep(1)
+            except NoSuchElementException:
+                continue
+
+            # Get the course sections
+            sections = []
+            section_divs = course.find_elements(By.XPATH, ".//div[@class='sections-container']//div[@class='section delivery-f2f']")
+            for section_div in section_divs:
+                try:
+                    section_number = section_div.find_element(By.XPATH, ".//span[@class='section-id']").text
+                except NoSuchElementException:
+                    section_number = "No Section Number"
+                try:
+                    instructor = section_div.find_element(By.XPATH, ".//span[@class='section-instructors']").text
+                except NoSuchElementException:
+                    instructor = "No Instructor"
+                try:
+                    seats = section_div.find_element(By.XPATH, ".//span[@class='seats-info']").text
+                except NoSuchElementException:
+                    seats = "No Seats"
+                try:
+                    section_time = section_div.find_element(By.XPATH, ".//div[@class='section-day-time-group push_two five columns']").text
+                except NoSuchElementException:
+                    section_time = "No Time"
+                try:
+                    location = section_div.find_element(By.XPATH, ".//span[@class='class-building']").text
+                except NoSuchElementException:
+                    location = "No Location"
+
+                # Create a Section object
+                s = Section(section_number, instructor, seats, section_time, location)
+                sections.append(s)
+
+            # Get the course data
+            try:
+                theCourse = course.find_element(By.XPATH, ".//div[@class='course-id']").text
+            except NoSuchElementException:
+                theCourse = "No Course ID"
+            try:
+                credits = course.find_element(By.XPATH, ".//span[@class='course-min-credits']").text
+            except NoSuchElementException:
+                credits = "No Credits"
+            try:
+                title = course.find_element(By.XPATH, ".//span[@class='course-title']").text
+            except NoSuchElementException:
+                title = "No Title"
+            try:
+                description = course.find_element(By.XPATH, './/div[contains(@class, "approved-course-text")][not(contains(text(), "Restriction:"))][not(contains(text(), "Cross-listed with:"))][not(contains(text(), "Credit only granted for:"))]').text
+            except NoSuchElementException:
+                description = "No Description"
+            try:
+                prerequisites = course.find_element(By.XPATH, ".//div[@class='twelve columns']//div[@class='approved-course-text']").text
+            except NoSuchElementException:
+                prerequisites = "No Prerequisites"
+
+            # Get the department from the department_list
+            department = department_name
+
+            # Create a Course object
+            c = Course(theCourse, title, credits, description, prerequisites, department, sections)
 
             # Check if any of the keywords are in the title or description
-            for keyword in words.keywords:
+            for keyword in keywords:
                 if keyword.lower() in title.lower() or keyword.lower() in description.lower():
-                    print(f"{department_name}: {title}")
+                    courses_data.append(c)
+                    db.add_course(c)
+                    for section in sections:
+                        db.add_section(section.section_number, theCourse, section.instructor, section.seats, section.section_time, section.location)
                     break
+
+            # Print the courses and sections in the database
+            db.cursor.execute("SELECT * FROM courses")
+            courses = db.cursor.fetchall()
+            for course in courses:
+                print(f"Course ID: {course[0]}, Title: {course[1]}, Credits: {course[2]}, Department: {course[5]}, Description: {course[3]}")
+                
+                db.cursor.execute("SELECT * FROM sections WHERE course_id=?", (course[0],))
+                sections = db.cursor.fetchall()
+                for section in sections:
+                    print(f"\tSection Number: {section[0]}, Instructor: {section[2]}, Seats: {section[3]}, Section Time: {section[4]}, Location: {section[5]} \n")
 
 def parse_args(args_list):
     """Takes a list of strings from the command prompt and passes them through as arguments
@@ -194,6 +267,7 @@ def parse_args(args_list):
     parser = argparse.ArgumentParser()
     
     parser.add_argument('keywords', metavar='keyword', type=str, nargs='+', help='keywords to search for')
+    parser.add_argument('-d', "--departments", metavar='D', type=str, nargs='+', help='list of department names to search for')
     
     args = parser.parse_args(args_list)
     
@@ -201,4 +275,4 @@ def parse_args(args_list):
 
 if __name__ == "__main__":
     arguments = parse_args(sys.argv[1:])
-    main(arguments.keywords)
+    main(arguments.keywords, arguments.departments)
